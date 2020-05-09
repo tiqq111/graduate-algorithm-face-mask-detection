23a278411a53fb71e031ee980e0c8189fbbc72f7
wangzhen@wavewisdom-bj.com <ANcheng988>
2020-05-09 18:17:32 +0800

    add video txt

--- example/main.cpp
+++ example/main.cpp
@@ -22,17 +22,13 @@ void readTxt(std::string file,std::vector<std::string> &image_pathes){
     infile.close();
 }
 
-int main(int argc, char** argv)
-{
-
-    string imgPath = argv[1];
-
+int detectFromTxt(std::string txt_path){
     // slim or RFB
     FaceDetection detector;
     // retinaface
     detector.Initial("../model/");
     std::vector<std::string> image_pathes;
-    readTxt(argv[1],image_pathes);
+    readTxt(txt_path,image_pathes);
 
     for	(int i = 0; i < image_pathes.size(); i++){
 
@@ -55,6 +51,7 @@ int main(int argc, char** argv)
             fprintf(stderr, "cv::imread %s failed\n", image_path.c_str());
             return -1;
         }
+
         FaceRect boxes;
 
         float score;
@@ -77,6 +74,61 @@ int main(int argc, char** argv)
         cv::putText(img, trackid, cv::Size((boxes.x/scale), boxes.y/scale + 15), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(255, 255, 0));
         cv::imwrite(std::string("./detect_result_1000/") + image_name.c_str(), img);
     }
+}
+
+int detetFromVideo(std::string videoPath){
+
+    cv::VideoCapture capture;
+    capture.open(videoPath);
+
+    float scale = 1.0;
+     // slim or RFB
+    FaceDetection detector;
+    // retinaface
+    detector.Initial("../model/");
+    int index = 1;
+    while(1){
+        cv::Mat img;
+
+        capture>>img;
+        cv::Mat img_scale = img;
+        
+        // cout<<img_scale<<endl;
+        if (!img_scale.data || img_scale.empty())
+        {
+            fprintf(stderr, "cv::imread %s failed\n");
+            return -1;
+        }
+        FaceRect boxes;
+
+        float score;
+        bool has_mask;
+        int track_id;
+        detector.DetectionMaxFace(img_scale.data, img_scale.cols,img_scale.rows,&boxes,&score,&has_mask,&track_id);
+
+        printf("track_id:%d\n",track_id);
+        cv::Rect rect(boxes.x/scale, boxes.y/scale, boxes.width/scale ,boxes.height/scale);
+        cv::rectangle(img, rect, cv::Scalar(0, 0, 255), 1, 8, 0);
+        char test[80];
+        sprintf(test, "%f",score);
+        printf("%f\n",score);
+        printf("has_mask:%d\n",has_mask);
+
+        char trackid[80];
+        sprintf(trackid, "%d",track_id);
+
+        cv::putText(img, test, cv::Size((boxes.x/scale), boxes.y/scale), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(255, 255, 0));
+        cv::putText(img, trackid, cv::Size((boxes.x/scale), boxes.y/scale + 15), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(255, 255, 0));
+        
+        cv::imwrite(std::string("./detect_result_1000/") + std::to_string(index) + ".jpg" , img);
+        index += 1;
+    }
+}
+int main(int argc, char** argv)
+{
+
+    
+    detetFromVideo("../data/test.mp4");
     return 0;
 }
 
